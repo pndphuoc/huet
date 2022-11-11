8090b244476da956eeafe4124d02d29daef3e111
pndphuoc <phamngocduyphuoc3132001@gmail.com>
2022-11-12 01:40:25 +0700

    create project

---
+++ android/app/src/main/kotlin/com/example/hue_t/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hue_t
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+library colors;
+
+import 'package:flutter/material.dart';
+
+var backgroundColor = Colors.white;
+var filterItemColor = Color.fromARGB(255, 246, 246, 246);
+var findButtonColor = Color.fromARGB(255, 39, 46, 50);
